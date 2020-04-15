bf8413af756daef9a86a3543c1a2ed83471110cf
ZAIvanov18 <58166571+ZAIvanov18@users.noreply.github.com>
2020-04-15 18:16:21 +0300

    Creating function:
    showCars, showAllCars, deleteCar, deleteCarMenu

--- GithubStructure.cpp
+++ GithubStructure.cpp
@@ -38,7 +38,67 @@ void insertCar(CARS car[], int& carCount)
 }
 
 
+void showCars(CARS car)
+{
+		cout << "\nManufacturer: " << car.manufacturer << endl;
+		cout << "Model: " << car.carModel << endl;
+		cout << "Price: " << car.carPrice << endl;
+		cout << "Year of creation: " << car.yearOfCreation << endl;
+		cout << endl;
+	
+}
 
+void showAllCars(CARS car[], int carCount)
+{
+
+	cout << "\nYou have entered the following cars:" << endl;
+
+	for (int i = 0; i < carCount; i++)
+	{
+		showCars(car[i]);
+	}
+}
+
+
+
+void deleteCar(CARS car[], int& carCount)
+{
+	string carModel;
+	int yearOfCreation;
+	string manufacturer;
+	int carPrice;
+
+	cout << "\nEnter Car Model: ";
+	cin >> carModel;
+
+	cout << "Enter price: ";
+	cin >> yearOfCreation;
+
+	cout << "Enter manufacturer: ";
+	cin >> manufacturer;
+
+	cout << "Enter Car Price: ";
+	cin >> carPrice;
+
+	for (int i = 0; i < carCount; i++) {
+
+		if (car[i].manufacturer == manufacturer && car[i].carModel == carModel && car[i].carPrice == carPrice && car[i].yearOfCreation == yearOfCreation) {
+
+			for (int i = 0; i < carCount; i++) {
+				car[i] = car[i + 1];
+			}
+
+		}
+
+		carCount--;
+	}
+
+}
+
+void deleteCarMenu(CARS car[], int& carCount)
+{
+	deleteCar(car, carCount);
+}
 
 int main()
 {
