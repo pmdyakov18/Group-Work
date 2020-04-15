3e7815f91116f52d352aeb7987b88cf678ed93ed
pmdyakov18 <59693549+pmdyakov18@users.noreply.github.com>
2020-04-15 18:13:12 +0300

    Creating Struct Cars and function insertCar

--- GithubStructure.cpp
+++ GithubStructure.cpp
@@ -4,7 +4,52 @@
 using namespace std;
 
 
+struct CARS
+{
+	string carModel;
+	int yearOfCreation;
+	string manufacturer;
+	int carPrice;
+  int ID;
+};
+
+void insertCar(CARS car[], int& carCount)
+{
+
+	cout << "\nEnter Car Model: ";
+	cin >> car[carCount].carModel;
+
+	cout << "\nEnter yearOfCreation: ";
+	cin >> car[carCount].yearOfCreation;
+
+	cout << "\nEnter manufacturer: ";
+	cin >> car[carCount].manufacturer;
+
+	cout << "\nEnter Car Price: ";
+	cin >> car[carCount].carPrice;
+
+  cout<<"Enter Car ID: ";
+  cin>>car[carCount].ID;
+
+
+
+	carCount++;
+
+}
+
+
+
+
 int main()
 {
-	
+	int carCount = 0;
+
+	CARS car[10];
+
+
+	bool doShowMenu = true;
+
+	do {
+		doShowMenu = showMainMenu(car, carCount);
+	} while (doShowMenu);
 }
